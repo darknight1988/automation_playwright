dfd9df80fe3290d65ea695f7595d9370dc085e03
danh@inspectorio.com <danh@inspectorio.com>
2023-11-23 14:53:46 +0700

    Getting rid of the unused code

---
+++ sources/pages/signIn.ts
@@ -0,0 +1,9 @@
+import { Page } from '@playwright/test';
+
+export default class SignInPage {
+  constructor(public page: Page) {}
+
+  /**
+    Define the main actions in the page as methods.
+  */
+}
